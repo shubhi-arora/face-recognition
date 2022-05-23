b68019f920f91fea40e2790708ca00c58dcab0b9
Shubhi Arora <79078776+shubhi-arora@users.noreply.github.com>
2022-05-23 17:45:26 +0530

    Add test.py and haarcascade_frontalface

---
+++ test.py
@@ -0,0 +1,38 @@
+from gettext import install
+import cv2
+from matplotlib import collections
+import numpy as np
+from deepface import DeepFace
+from collections import Counter
+import matplotlib.pyplot as plt
+
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
+# if not cap.isOpened():
+#     cap = cv2.VideoCapture(1)
+# if not cap.isOpened():
+#     raise IOError("Cannot open Webcam")
+l=[];
+while True:
+        ret,frame = cap.read()
+        result = DeepFace.analyze(frame,actions=['emotion'],enforce_detection=False)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(gray, 1.32, 5)
+        for(x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            
+        font = cv2.FONT_HERSHEY_DUPLEX
+        l.append(result['dominant_emotion'])
+        cv2.putText(frame, result['dominant_emotion'],(100, 100), font, 5, (0, 0, 255), 12)
+        
+        cv2.imshow('Video', frame)
+        if cv2.waitKey(2) == ord('q'):  # wait until 'q' key is pressed
+          break
+emotion={"neutral":l.count("neutral"),"happy":l.count("happy"),"sad":l.count("sad"),"angry":l.count("angry"),"fear":l.count("fear"),"surprise":l.count("surprise"),"disgust":l.count("disgust")}
+v=list(emotion.values())
+k=list(emotion.keys())
+print(k[v.index(max(v))])
+print(max(v))
+
+cap.release()
+cv2.destroyAllWindows()
