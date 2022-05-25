687bfe0ca1496e8114bebf6599e4793368759778
shubhi-arora <arora.shubhi0565@gmail.com>
2022-05-25 20:31:25 +0530

    Updates to index file

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+##################################  Importing Libraries      ###########################################
+
 from flask import Flask,render_template
 from gettext import install
 import cv2
@@ -8,33 +10,39 @@ from collections import Counter
 import matplotlib.pyplot as plt
 app=Flask(__name__)
 @app.route("/")
+
+#################################   Render index.html page   #############################################
+
 @app.route("/home")
 def home():
     return render_template("index.html")
 @app.route("/result",methods=['POST',"GET"])
 def result():
     faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+#################################  Turn on the camera and capture the image  ##############################
     cap = cv2.VideoCapture(0)
     l=[];
     while True:
         ret,frame = cap.read()
+#################################   Detect the emotions    ####################################
         result = DeepFace.analyze(frame,actions=['emotion'],enforce_detection=False)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = faceCascade.detectMultiScale(gray, 1.32, 5)
         for(x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 255), 4)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 255), 4)  #Put rectangle over the face
             
         font = cv2.FONT_HERSHEY_SIMPLEX
-        l.append(result['dominant_emotion'])
+        l.append(result['dominant_emotion'])  # Insert the dominant emotion into list
         cv2.putText(frame, result['dominant_emotion'],(100, 100), font, 4, (0, 0, 255), 9)
         
         cv2.imshow('Video', frame)
         if cv2.waitKey(2) == ord('q'):  # wait until 'q' key is pressed
            break
+    # Make a map of emotions with their count
     emotion={"neutral":l.count("neutral"),"happy":l.count("happy"),"sad":l.count("sad"),"angry":l.count("angry"),"fear":l.count("fear"),"surprise":l.count("surprise"),"disgust":l.count("disgust")}
     v=list(emotion.values())
     k=list(emotion.keys())
-    print(k[v.index(max(v))])
+    print(k[v.index(max(v))])  # Detect the emotion with the maximum count
     print(max(v))
     l=[]
     cap.release()
@@ -42,5 +50,5 @@ def result():
     return {k[v.index(max(v))]}
 
 if(__name__)=='__main__':
-    app.run(debug=True,port=5001)
+    app.run(debug=True,port=5001)  # Run on localhost 5001
 
