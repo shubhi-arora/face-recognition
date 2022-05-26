ad501e3b508cda7abeb368ac8520e892ba6b8c71
shubhi-arora <arora.shubhi0565@gmail.com>
2022-05-26 22:30:10 +0530

    Update in app.py and add about.html

--- app.py
+++ app.py
@@ -16,6 +16,9 @@ app=Flask(__name__)
 @app.route("/home")
 def home():
     return render_template("index.html")
+@app.route("/about",methods=['GET'])
+def about():
+    return render_template("about.html")   
 @app.route("/result",methods=['POST',"GET"])
 def result():
     faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
@@ -29,7 +32,7 @@ def result():
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = faceCascade.detectMultiScale(gray, 1.32, 5)
         for(x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 255), 4)  #Put rectangle over the face
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 255), 4)  # Put rectangle over the face
             
         font = cv2.FONT_HERSHEY_SIMPLEX
         l.append(result['dominant_emotion'])  # Insert the dominant emotion into list
@@ -47,7 +50,7 @@ def result():
     l=[]
     cap.release()
     cv2.destroyAllWindows()
-    return {k[v.index(max(v))]}
+    return k[v.index(max(v))],max(v)
 
 if(__name__)=='__main__':
     app.run(debug=True,port=5001)  # Run on localhost 5001
